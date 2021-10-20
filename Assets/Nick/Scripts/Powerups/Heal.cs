78fdf15df83a388d74de38f3dae116446a2bf66f
Nick12345-code <astonishedchocolate@gmail.com>
2021-10-20 11:08:17 +1100

    powerup setup

---
+++ Assets/Nick/Scripts/Powerups/Heal.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Heal : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        // player recovers 1 health point by picking up this powerup
+    }
+}
---
+++ Assets/Nick/Scripts/Powerups/PowerupSpawner.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class PowerupSpawner : MonoBehaviour
+{
+    // create a spawner that randomly spawns a random powerup
+    // another powerup can only spawn once the previous one has dissapeared or been picked up
+    //
+}
